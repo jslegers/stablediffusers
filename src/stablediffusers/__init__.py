68e3c298d892c1f5ded90d886e63950ab430d6ae
John Slegers <slegersjohn@gmail.com>
2024-08-01 07:44:12 +0200

    Test
    test

--- src/stablediffusers/__init__.py
+++ src/stablediffusers/__init__.py
@@ -1,15 +1,13 @@
 from sys import modules
 from os.path import join, dirname
 from .util import (
-#    LazyModule,
+    LazyModule,
     import_structure
 )
 
-import_test = import_structure(join(dirname(globals()["__file__"]), "class"))
-
-#modules[__name__] = LazyModule(
-#  __name__,
-#  globals()["__file__"],
-#  import_structure(join(getcwd(), "class")),
-#  module_spec = __spec__
-#)
+modules[__name__] = LazyModule(
+  __name__,
+  globals()["__file__"],
+  import_structure(join(dirname(globals()["__file__"]), "class")),
+  module_spec = __spec__
+)
