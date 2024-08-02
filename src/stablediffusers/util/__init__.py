7310b6d82e015043e63c9e148f761b1ea8b7a0d7
John Slegers <slegersjohn@gmail.com>
2024-08-02 04:57:08 +0200

    Test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -33,9 +33,9 @@ def all_files_in_path(*args, **kwargs) :
       if entry.is_dir() :
         kwargs["path_from_package"] = join(path_from_package, entry.name)
         print(f"TEST : {skip_internal_package}")
-        print(f"TEST : {kwargs["path_from_package"]}")
-        print(f"TEST : {join(kwargs["path_from_package"], package_file)}")
-        print(f"TEST : {isfile(join(kwargs["path_from_package"], package_file))}")
+        print(f"TEST : {kwargs['path_from_package']}")
+        print(f"TEST : {join(kwargs['path_from_package'], package_file)}")
+        print(f"TEST : {isfile(join(kwargs['path_from_package'], package_file))}")
         if not skip_internal_package or not isfile(join(kwargs["path_from_package"], package_file)) :
           dict.update(all_files_in_path(package_path, **kwargs))
       elif entry.name not in exclude_files :
