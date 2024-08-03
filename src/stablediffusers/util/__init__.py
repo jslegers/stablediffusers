789246a59ada1642ca7c5c5ee8741174116f668a
John Slegers <slegersjohn@gmail.com>
2024-08-03 13:07:44 +0200

    test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -69,7 +69,7 @@ def caller_name(depth = 2):
 
      https://gist.github.com/techtonik/2151727
   """
-  stack = get_stack()
+  stack = get_stack(depth)
   start = 0 + depth
   if len(stack) < start + 1:
     raise Exception("Stack limit reached")
