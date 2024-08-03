b2e95da329242bdedd9058942d50d47d64e51f6e
John Slegers <slegersjohn@gmail.com>
2024-08-03 11:47:12 +0200

    test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -11,6 +11,8 @@ import inspect
 
 def caller_info():
   previous_frame = None
+  previous_frame = inspect.getmodule(inspect.getouterframes(inspect.currentframe(), 2)[2])
+  return module
   try :
     previous_frame = _getframe(2)
   except Exception :
