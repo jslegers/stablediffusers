d0d2d159f6e38a40e642ee3ac42dffaf32754950
John Slegers <slegersjohn@gmail.com>
2024-08-03 19:40:53 +0200

    test
    test

--- src/stablediffusers/class/ComposableStableDiffusionXLPipeline.py
+++ src/stablediffusers/class/ComposableStableDiffusionXLPipeline.py
@@ -1,6 +1,22 @@
-from stablediffusers import util
-from torch import bfloat16, float16, device as dev, Generator
+from stablediffusers.util import AutoLoad
+AutoLoad(import_structure = {
+  "torch" : ["bfloat16", "float16", "device"],
+  "torch.cuda" : ["is_available", "ipc_collect", "empty_cache"],
+  "numba.cuda" : ["select_device", "get_current_device"],
+  "gc" : ["collect"],
+  "accelerate" : ["init_empty_weights"],
+  "huggingface_hub" : ["hf_hub_download", "snapshot_download"],
+  "diffusers.models.model_loading_utils" : ["load_model_dict_into_meta"],
+  "diffusers.utils" : ["logging"],
+  "diffusers" : ["StableDiffusionXLPipeline", "UNet2DConditionModel", "AutoencoderKL"],
+  "transformers" : ["CLIPTextModel", "CLIPTextModelWithProjection"],
+  "sd_embed.embedding_funcs" : ["get_weighted_text_embeddings_sdxl"],
+  "PIL" : ["Image", "ImageDraw", "ImageFont"],
+  "os.path" : ["join"]
+})
 # bfloat16, float16, dev, Generator = util.from_module('torch').load('bfloat16', 'float16', 'device', 'Generator')
+"""
+from torch import bfloat16, float16, device as dev, Generator
 from torch.cuda import is_available, ipc_collect, empty_cache
 from numba.cuda import select_device, get_current_device
 from gc import collect
@@ -13,7 +29,9 @@ from transformers import CLIPTextModel, CLIPTextModelWithProjection
 from sd_embed.embedding_funcs import get_weighted_text_embeddings_sdxl
 from PIL import Image, ImageDraw, ImageFont
 from os.path import join
+"""
 import cv2
+dev = device
 
 class ComposableStableDiffusionXLPipeline:
 
