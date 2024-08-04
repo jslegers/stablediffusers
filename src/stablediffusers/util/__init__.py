36847569d157a096c47cd408147fd6f719a7a050
John Slegers <slegersjohn@gmail.com>
2024-08-04 14:32:54 +0200

    test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -146,9 +146,9 @@ def lazy(fullname):
   try:
     return sys.modules[fullname]
   except KeyError:
-    spec = importlib.util.find_spec(fullname)
-    module = importlib.util.module_from_spec(spec)
-    loader = importlib.util.LazyLoader(spec.loader)
+    spec = util.find_spec(fullname)
+    module = util.module_from_spec(spec)
+    loader = util.LazyLoader(spec.loader)
     # Make module with proper locking and get it inserted into sys.modules.
     loader.exec_module(module)
     return module
