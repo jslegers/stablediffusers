716840f5a15f5d260d057aff3e3d18cea32937ec
John Slegers <slegersjohn@gmail.com>
2024-08-04 02:30:44 +0200

    test
    test

--- src/stablediffusers/class/ComposableStableDiffusionXLPipeline.py
+++ src/stablediffusers/class/ComposableStableDiffusionXLPipeline.py
@@ -17,22 +17,6 @@ module = AutoLoad(import_structure = {
   "os.path" : ["join"]
 })
 
-# bfloat16, float16, dev, Generator = util.from_module('torch').load('bfloat16', 'float16', 'device', 'Generator')
-"""
-from torch import bfloat16, float16, device as dev, Generator
-from torch.cuda import is_available, ipc_collect, empty_cache
-from numba.cuda import select_device, get_current_device
-from gc import collect
-from accelerate import init_empty_weights
-from huggingface_hub import hf_hub_download, snapshot_download
-from diffusers.models.model_loading_utils import load_model_dict_into_meta
-from diffusers.utils import logging
-from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, AutoencoderKL
-from transformers import CLIPTextModel, CLIPTextModelWithProjection
-from sd_embed.embedding_funcs import get_weighted_text_embeddings_sdxl
-from PIL import Image, ImageDraw, ImageFont
-from os.path import join
-"""
 import cv2
 dev = module.load('device')
 
