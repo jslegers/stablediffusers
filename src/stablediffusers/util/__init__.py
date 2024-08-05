c902b4d4a6ae2cf2cd586d6143e023cf334dabf9
John Slegers <slegersjohn@gmail.com>
2024-08-05 09:01:41 +0200

    test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -593,7 +593,10 @@ def module(name, attrs = None) :
 
     def __getattr__(self, key):
       self._Module_Attr__PROXY__activate()
-      return Module_proxy._Module_Attr__module[key]
+      if key in Module_proxy._Module_Attr__module
+        return Module_proxy._Module_Attr__module[key]
+      else
+        return Module_proxy._Module_Attr__module[Module_proxy.attr_names[0]][key]
 
     def __getitem__(self, key):
       return type(self).attrs[key]
