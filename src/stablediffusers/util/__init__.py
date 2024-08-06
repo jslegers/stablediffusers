6c6eadef1d6192f31d470594c6b5bc7429c9a468
John Slegers <slegersjohn@gmail.com>
2024-08-06 06:57:23 +0200

    test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -357,7 +357,7 @@ def module(name, attrs = None) :
       if not Module_proxy._Module_Attr__PROXY__activated :
         Module_proxy._Module_Attr__PROXY__activated = True
         print("ACTIVATE")
-        mod = get_mod(Module_proxy.MODULY_PROXY_name, cls.attr_names)
+        mod = get_mod(Module_proxy_parent.MODULY_PROXY_name, cls.attr_names)
         try :
           mod = next(mod)
         except :
@@ -381,13 +381,10 @@ def module(name, attrs = None) :
           Module_proxy._Module_Attr__module
 
     def __init__(self, name) :
-      Module_proxy.MODULY_PROXY_name = name
+      self.MODULY_PROXY_name = name
 
   class Module_proxy_child(Module_proxy):
     @classmethod
-
-    def __init__(self, name) :
-      self.MODULY_PROXY_name = name
 
     def __getattr__(self, key):
       self._Module_Attr__PROXY__activate()
