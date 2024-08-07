6bc99321c40af729a87472a8d075e54ca629a15b
John Slegers <slegersjohn@gmail.com>
2024-08-07 16:22:54 +0200

    test
    test

--- src/stablediffusers/util/__init__.py
+++ src/stablediffusers/util/__init__.py
@@ -436,7 +436,10 @@ def module(module, attrs = None) :
     attrs = [attrs]
   if isinstance(module, str) :
     try :
-      module = sys.modules[module]
+      module = sys.modules[module] and get_mod(module, attrs)
+      print(module)
+      pprint.pp(module.__dict__)
+      print(attrs)
       return ((key, module.__dict__[key]) for key in attrs)
     except Exception as e :
       return Module_proxy(module, attrs)
